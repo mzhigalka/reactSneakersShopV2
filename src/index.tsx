df548adf84786eb2774bc08050c7b792f5c1d4b4
Matvey Zhigalka <mzhigalka@gmail.com>
2024-05-24 15:53:39 +0300

    last update

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,11 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import "../src/scss/index.scss";
-import React from "react";
 import App from "./App";
 import "macro-css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>
+  <Router>
+    <App />
+  </Router>
 );
